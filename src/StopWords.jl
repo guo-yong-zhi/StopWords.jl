b47d8dfbd7c81321928778fdbde1be777bc77f7f
guoyongzhi <guoyongzhi@megvii.com>
2023-09-04 23:21:57 +0800

    support index by list

--- src/StopWords.jl
+++ src/StopWords.jl
@@ -6,27 +6,44 @@ export stopwords
 const PACKAGE_PATH = pkgdir(StopWords)
 const DATA_PATH = joinpath(PACKAGE_PATH, "stopwords")
 
-struct StopWordsDict <: AbstractDict{String,Set{String}}
-    dict::Dict{String,Set{String}}
+struct StopWordsDict <: AbstractDict{Any, Set{String}}
+    dict::Dict{String, Set{String}}
+    dict2::Dict{Vector{String}, Set{String}}
     languages::Set{String}
     path::String
 end
 function StopWordsDict(path=DATA_PATH)
     languages = Set([f[1:end-4] for f in readdir(path)])
-    return StopWordsDict(Dict{String,Set{String}}(), languages, path)
+    return StopWordsDict(Dict{String, Set{String}}(), Dict{Vector{String}, Set{String}}(),languages, path)
 end
-function Base.iterate(sw::StopWordsDict, state=1)
-    iterate(sw.dict, state)
+function Base.iterate(sw::StopWordsDict, st=(1, nothing))
+    if first(st) == 1
+        res = last(st) === nothing ? iterate(sw.dict) : iterate(sw.dict, last(st))
+        if res !== nothing
+            v, s = res
+            return v, (1, s)
+        else
+            return iterate(sw, (2, nothing))
+        end
+    else
+        res = last(st) === nothing ? iterate(sw.dict2) : iterate(sw.dict2, last(st))
+        if res !== nothing
+            v, s = res
+            return v, (2, s)
+        else
+            return nothing
+        end
+    end
 end
 function Base.length(sw::StopWordsDict)
-    length(sw.dict)
+    length(sw.dict) + length(sw.dict2)
 end
 function Base.getindex(sw::StopWordsDict, lang::String)
     if haskey(sw.dict, lang)
         return sw.dict[lang]
     else
         if lang in sw.languages
-            return sw.dict[lang] = load_stopwords(norm_lang, path=sw.path)
+            return sw.dict[lang] = load_stopwords(lang, path=sw.path)
         end
         norm_lang = normcode(lang)
         if haskey(sw.dict, norm_lang)
@@ -40,16 +57,35 @@ function Base.getindex(sw::StopWordsDict, lang::String)
         end
     end
 end
-
+function Base.getindex(sw::StopWordsDict, langs::Vector{String})
+    if haskey(sw.dict2, langs)
+        return sw.dict2[langs]
+    else
+        norm_langs = sort(unique([normcode(lang) for lang in langs]))
+        if haskey(sw.dict2, norm_langs)
+            return sw.dict2[langs] = sw.dict2[norm_langs]
+        else
+            if length(norm_langs) == 1
+                return sw.dict2[langs] = sw.dict2[norm_langs] = sw[norm_langs[1]]
+            else
+                return sw.dict2[langs] = sw.dict2[norm_langs] = union([sw[lang] for lang in norm_langs]...)
+            end
+        end
+    end
+end
 """
     haskey(sw::StopWordsDict, lang::String) -> Bool
+    haskey(sw::StopWordsDict, langs::Vector{String}) -> Bool
 
 Check if `lang` is a supported language. You can use either the language's name or its ISO 639 code.
 For example, `haskey(stopwords, "en")`, `haskey(stopwords, "eng")`, and `haskey(stopwords, "English")` all return `true`.
+And `haskey(stopwords, ["en", "fr"])` returns `true` if both English and French are supported.
 """
 function Base.haskey(sw::StopWordsDict, lang::String)
     haskey(sw.dict, lang) || lang in sw.languages || normcode(lang) in sw.languages
 end
+Base.haskey(sw::StopWordsDict, langs::Vector{String}) = all(haskey(sw, lang) for lang in langs)
+
 
 function load_stopwords(lang; path=DATA_PATH)
     filename = joinpath(path, "$lang.txt")
@@ -62,6 +98,7 @@ end
 `stopwords` is a lazy dictionary of stop words for multiple languages. 
 You can access the stop words for a given language using the language's name or ISO 639 code.
 For example, to get the stop words for English, you can use `stopwords["eng"]`, `stopwords["en"]`, or `stopwords["English"]`.
+And to get the stop words for multiple languages, you can use a vector, e.g. `stopwords[["en", "fr"]]`.
 """
 const stopwords = StopWordsDict(DATA_PATH)
 
--- test/runtests.jl
+++ test/runtests.jl
@@ -5,9 +5,18 @@ using Test
     @test isempty(stopwords)
     println(stopwords)
     @test stopwords["en"] === stopwords["eng"] === stopwords["English"]
+    @test stopwords["fra"] === stopwords["fra"] === stopwords["French"] === stopwords["fr"]
     @test !isempty(stopwords["zh"])
     @test !isempty(stopwords)
     @test haskey(stopwords, "en")
     @test haskey(stopwords, "French")
     @test haskey(stopwords, "zho")
+    @test haskey(stopwords, ["zh"])
+    @test haskey(stopwords, ["zh", "French"])
+    @test !haskey(stopwords, "balabala")
+    @test !haskey(stopwords, ["balabala", "French"])
+    @test stopwords[["en", "Chinese"]] === stopwords[["zho", "eng"]]
+    @test stopwords[["en", "French"]] == stopwords["French"] ∪ stopwords["English"]
+    @test stopwords[["en", "eng", "en"]] === stopwords["English"] === stopwords[["eng"]]
+    println(keys(stopwords))
 end
